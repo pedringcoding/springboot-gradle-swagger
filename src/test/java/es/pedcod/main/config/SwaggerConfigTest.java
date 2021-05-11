035def98807c55b14e7a9678c299e226957cfa59
pfernandezc <pfernandezc@indra.es>
2021-05-11 23:38:11 +0200

    feat: apply unit test dependency configurations for OpenApi config

---
+++ src/test/java/es/pedcod/main/config/SwaggerConfigTest.java
@@ -0,0 +1,44 @@
+package es.pedcod.main.config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.Alphanumeric;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * Unit Test Class for configuration class
+ */
+@DisplayName("Configuration to OpenAPI")
+@TestMethodOrder(Alphanumeric.class)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter(AccessLevel.PRIVATE)
+@Setter(AccessLevel.PRIVATE)
+class SwaggerConfigTest {
+
+	/**
+	 * The principal component to test
+	 */
+	SwaggerConfig configuration;
+
+	@Nested
+	@DisplayName("Docket Bean")
+	class testDocket {
+
+		@Test
+		@DisplayName("Default instance must not be null")
+		void testExistsElements() {
+			final Docket actualBean = getConfiguration().getApiBean();
+
+			assertThat(actualBean).isNotNull();
+		}
+	}
+}
